27b4c65796788a1b4f7158fa5fc75d013dd3ea17
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-07-24 18:52:31 +0600

    Project Setup Done

---
+++ src/app/layout.js
@@ -0,0 +1,37 @@
+import { Roboto_Slab, Patua_One } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/context/ThemeContext";
+
+const robotoSlab = Roboto_Slab({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    variable: "--font-roboto-slab",
+    display: "swap",
+});
+
+const patuaOne = Patua_One({
+    subsets: ["latin"],
+    weight: ["400"],
+    variable: "--font-patua",
+    display: "swap",
+});
+
+export const metadata = {
+    title: "M EDUHUB",
+    description: "",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html
+            lang="en"
+            className={`${robotoSlab.variable} ${patuaOne.variable} h-full antialiased`}
+        >
+            <body className="min-h-full">
+                <ThemeProvider>
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
